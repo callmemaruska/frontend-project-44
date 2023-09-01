b491fb1182c94c9e13dd648f03cf4fa51465ebdb
Maria Shanaeva <callmemaruska@proton.me>
2023-09-01 17:57:21 +0300

    all updates

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,5 +1,5 @@
 function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-};
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 export default generateRandomNumber;
