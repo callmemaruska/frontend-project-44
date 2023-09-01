e2ec39c5cd39f05e4d3a721ad73090aadc086ec0
Maria Shanaeva <callmemaruska@proton.me>
2023-09-01 18:21:49 +0300

    add cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greetUser = () => {
-  const userName = readlineSync.question('Welcome to the brain games!\nMay I have your name? ');
+  console.log(`Welcome to the brain games!`);
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
